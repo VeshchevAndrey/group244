910d21a85a1b1b6aa492cb6198664cf185ee048e
Andrey Veshchev <134205601+VeshchevAndrey@users.noreply.github.com>
2026-03-19 09:41:56 +0300

    Update DataClassApp.kt

--- Jetpack Compose/DataClassApp.kt
+++ Jetpack Compose/DataClassApp.kt
@@ -54,7 +54,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.application244.Companion
 
 
 // Точка сборки и запуска окна мобильного приложения
